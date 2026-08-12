b69ed752571eb68ac9c7377e8655d8c160e825ae
unknown <mdshahilofficial@gmail.com>
2026-08-12 21:30:08 +0530

    subnet module added

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -8,6 +8,15 @@ module "vnet" {
   source        = "../../module/vnet"
   vnet_name     = var.vnet_name
   location      = var.location
-  rg_name       = module.rg.rg_name
+  rg_name       = var.rg_name
   address_space = var.address_space
+}
+
+module "subnet" {
+  source          = "../../module/subnet"
+  depends_on      = [module.vnet]
+  subnet_name     = var.subnet_name
+  rg_name         = var.rg_name
+  vnet_name       = var.vnet_name
+  address_prefixes = var.subnet_prefixes
 }
--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -17,3 +17,13 @@ variable "address_space" {
   description = "Address space of the virtual network"
   type        = list(string)
 }
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_prefixes" {
+  description = "Address prefixes for the subnet"
+  type        = list(string)
+}
