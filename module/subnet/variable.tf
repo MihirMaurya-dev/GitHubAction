4af49cc822633a6e3212f2fe9f22f18c6fec135d
unknown <mdshahilofficial@gmail.com>
2026-08-12 21:56:43 +0530

    variable is fixed now

--- module/subnet/variable.tf
+++ module/subnet/variable.tf
@@ -1,10 +1,19 @@
 variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
 }
 
 variable "rg_name" {
+  description = "Name of the resource group for the subnet"
+  type        = string
 }
 
 variable "vnet_name" {
+  description = "Name of the virtual network where the subnet will be created"
+  type        = string
 }
 
-variable "address_prefixes" {}
+variable "address_prefixes" {
+  description = "List of address prefixes for the subnet"
+  type        = list(string)
+}
