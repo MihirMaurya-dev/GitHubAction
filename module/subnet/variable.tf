594cfd2fdbea3110e6425ccbd0eb830f0ac7b2cb
unknown <mdshahilofficial@gmail.com>
2026-08-12 21:35:01 +0530

    varibale is updated

--- module/subnet/variable.tf
+++ module/subnet/variable.tf
@@ -1,19 +1,10 @@
 variable "subnet_name" {
-  description = "Name of the subnet"
-  type        = string
 }
 
 variable "rg_name" {
-  description = "Name of the resource group for the subnet"
-  type        = string
 }
 
 variable "vnet_name" {
-  description = "Name of the virtual network where the subnet will be created"
-  type        = string
 }
 
-variable "address_prefixes" {
-  description = "List of address prefixes for the subnet"
-  type        = list(string)
-}
+variable "address_prefixes" {}
